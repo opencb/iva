085567719fef93622367a740a6611205d973a8b1
imedina <igmecas@gmail.com>
2016-07-29 15:28:29 +0100

    Major refactoring, the development from BierApp has been migrated to IVA

--- src/config.js
+++ src/config.js
@@ -14,14 +14,108 @@
  * limitations under the License.
  */
 
-var TITLE = "IVA Browser";
-var HOST = "aa";
-var USER_ID;
-var PROJECT = "b";
-var STUDIES = ["Study A", "Study B", "Study C"];
-var SETTINGS_SHOW = "true";
-var ABOUT = [{"name": "About", "url": "https://github.com/opencb/iva/wiki"}, 
-    {"name": "GitHub", "url": "https://github.com/opencb/iva"}, 
-    {"name": "Documentation",  "url": "https://github.com/opencb/iva/wiki"}];
-var LOGIN_SHOW = "false";
+var cellbase = {
+    hosts: ["http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0"],
+    version: "v4"
+};
 
+var opencga = {
+    host: "bioinfodev.hpc.cam.ac.uk/hgva",
+    version: "v1",
+    // user: "hgva",
+    // projects: ["hsapiens_grch37"],
+    cookies: {
+        userName: "opencga_userId",
+        sessionId: "opencga_sId"
+    }
+};
+
+var application = {
+    title: "BiERapp",
+    version: "v2.0.0",
+    logo: "images/bier-text.svg",
+    menu: [
+        {
+            id: "browser",
+            title: "Variant Browser",
+            visibility: "public"
+        },
+        {
+            id: "prioritization",
+            title: "Prioritization",
+            visibility: "public"
+        },
+        {
+            id: "diagnose",
+            title: "Diagnose",
+            visibility: "public",
+            submenu: [
+                {
+                    id: "diagnose:sample",
+                    title: "Sample",
+                    visibility: "public"
+                },
+                {
+                    id: "diagnose:family",
+                    title: "Family",
+                    visibility: "public"
+                }]
+        },
+        {
+            id: "beacon",
+            title: "Beacon",
+            visibility: "public"
+        },
+        {
+            id: "analysis",
+            title: "Analysis",
+            visibility: "public",
+            submenu: [
+                {
+                    id: "ibs",
+                    title: "IBS",
+                    visibility: "public"
+                },
+                {
+                    id: "burden",
+                    title: "Burden Test",
+                    visibility: "public"
+                }]
+        },
+        {
+            id: "tools",
+            title: "Tools",
+            visibility: "public",
+            submenu: [
+                {
+                    id: "genomeBrowser",
+                    title: "Genome Browser",
+                    visibility: "public"
+                },
+                {
+                    id: "exporter",
+                    title: "Exporter",
+                    visibility: "public"
+                }
+            ]
+        }
+    ],
+    search: {
+        placeholder: "eg. BRCA2",
+        visibility: "public"
+    },
+    settings: {
+        visibility: "public"
+    },
+    about: [
+        {"name": "Documentation",  "url": "https://github.com/babelomics/bierapp/wiki", "icon": "fa fa-book"},
+        {"name": "Tutorial", "url": "https://github.com/babelomics/bierapp/wiki/Tutorial", "icon": ""},
+        {"name": "Source code", "url": "https://github.com/babelomics/bierapp", "icon": "fa fa-github"},
+        {"name": "Contact",  "url": "", "icon": "fa fa-envelope"},
+        {"name": "FAQ",  "url": "", "icon": ""}
+    ],
+    breadcrumb: {
+        title: "Projects",
+        visibility: "public"
+    }
+};
