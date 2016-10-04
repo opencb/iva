5413d8fd22b354cdc30f892cf4b1e7a299ccf28c
imedina <igmecas@gmail.com>
2016-10-05 00:51:40 +0100

    Fix highcharts import

--- src/config.js
+++ src/config.js
@@ -21,6 +21,7 @@ var cellbase = {
 
 var opencga = {
     host: "bioinfodev.hpc.cam.ac.uk/hgva",
+    // host: "localhost:9090/opencga",
     version: "v1",
     // user:  Useful scenario is user@project:study
     projects: [
