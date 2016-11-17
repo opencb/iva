6f5c2ff685d4e3369d3d75a868cdf33e67a123fb
Swaathik <swaathik@gmail.com>
2016-11-17 15:07:23 +0000

    README updated

--- src/config.js
+++ src/config.js
@@ -22,6 +22,7 @@ var cellbase = {
 var opencga = {
     // host: "bioinfodev.hpc.cam.ac.uk/opencga-1.0.0-rc3",
     host: "bioinfodev.hpc.cam.ac.uk/hgva",
+    // host: "localhost:9190/opencga",
     version: "v1",
     // user:  Useful scenario is user@project:study
     projects: [
