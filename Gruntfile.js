c553044cf0a3d7c23d3ef7be2b32fce8b995f29d
imedina <igmecas@gmail.com>
2017-02-06 22:17:07 +0000

    Upgrade JSorolla dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,8 @@ module.exports = function(grunt) {
                     './lib/jsorolla/src/lib/clients/opencga-client-config.js',
                     './lib/jsorolla/src/lib/clients/opencga-client.js',
                     './lib/jsorolla/src/lib/cache/indexeddb-cache.js',
-                    './lib/jsorolla/src/lib/utils.js'
+                    './lib/jsorolla/src/lib/utils.js',
+                    './lib/jsorolla/src/lib/svg.js'
                 ],
                 dest: '<%= build.path %>/jsorolla-clients.js'
             },
