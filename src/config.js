4998f9593352a569f54aeae15c973c7b9087b322
Swaathik <swaathik@gmail.com>
2017-03-24 14:24:01 +0000

    Facet queries support is added

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ var cellbase = {
 
 var opencga = {
     // host: "bioinfodev.hpc.cam.ac.uk/opencga-1.0.0-rc3",
-    host: "bioinfodev.hpc.cam.ac.uk/hgva-1.0.0",
+    host: "bioinfodev.hpc.cam.ac.uk/hgva-1.0.0-facet",
     version: "v1",
     // user:  Useful scenario is user@project:study
     projects: [
