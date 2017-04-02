1a20065930e2c2c2ad8a3bb74f40ab94424efd60
imedina <igmecas@gmail.com>
2017-04-02 23:11:07 +0100

    config: make beacon private

--- src/config.js
+++ src/config.js
@@ -64,7 +64,7 @@ var application = {
         {
             id: "beacon",
             title: "Beacon",
-            visibility: "public"
+            visibility: "private"
         },
         {
             id: "analysis",
