5656982094ad777910fef8313a77817b7cf7fd36
Swaathik <swaathik@gmail.com>
2017-04-07 12:27:58 +0100

    JSorolla dependencu upgrade

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ var cellbase = {
 
 var opencga = {
     // host: "bioinfodev.hpc.cam.ac.uk/opencga-1.0.0-rc3",
-    host: "bioinfodev.hpc.cam.ac.uk/hgva-1.0.0-facet",
+    host: "bioinfodev.hpc.cam.ac.uk/hgva-1.1.0-dev",
     version: "v1",
     // asUser: "researchcga", // user@project:study
     projects: [
