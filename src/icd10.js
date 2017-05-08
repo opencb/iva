bc42a66cf6b04e53c47f7102312be04d57c8f162
Javier Perez-Florido <jpflorido@gmail.com>
2017-05-08 22:47:01 +0200

    jsorolla dependency upgraded. ICD10 json object added

---
+++ src/icd10.js
@@ -0,0 +1,73 @@
+ICD_10=[
+    {
+        text: "I.Certain infectious and parasitic diseases",
+        nodes:
+            [
+                {
+                    text: " A00-A09.Intestinal infectious diseases",
+                    nodes:
+                        [
+                            {
+                                text: "  A00.Cholera"
+                            },
+                            {
+                                text: "  A01.Typhoid and paratyphoid fevers"
+                            },
+                        ]
+                },
+                {
+                    text: " A15-A19.Tuberculosis",
+                    nodes:
+                        [
+                            {
+                                text: "  A15.Respiratory tuberculosis, bacteriologically and histologically confirmed"
+                            },
+                            {
+                                text: "  A16.Respiratory tuberculosis, not confirmed bacteriologically or histologically"
+                            },
+                        ]
+                }
+
+
+            ],
+
+
+    },
+    {
+        text: "II.Neoplasms",
+        nodes:
+            [
+                {
+                    text: " C00-C97.Malignant neoplasms",
+                    nodes:
+                        [
+                            {
+                                text: "  C00. Malignant neoplasm of lip"
+                            },
+                            {
+                                text: "  C01. Malignant neoplasm of base of tongue"
+                            },
+                        ]
+                },
+                {
+                    text: " D00-D09.In situ neoplasms",
+                    nodes:
+                        [
+                            {
+                                text: "  D00.Carcinoma in situ of oral cavity, oesophagus and stomach"
+                            },
+                            {
+                                text: "  D01.Carcinoma in situ of other and unspecified digestive organs"
+                            },
+                        ]
+                }
+
+
+            ],
+
+
+    },
+
+
+]
+
