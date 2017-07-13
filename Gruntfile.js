db7888f7548e84b8b7fa84be6e5e3b8cc7c5820e
imedina <igmecas@gmail.com>
2017-07-14 00:11:31 +0100

    Some fixes in Grunt build tool, it does not work well yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,20 +24,20 @@ module.exports = function(grunt) {
             },
             vendors: {
                 src: [
-                    "./bower_components/webcomponentsjs/webcomponents-lite.js",
+                    "./node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js",
                     "./node_modules/jquery/dist/jquery.js",
                     "./node_modules/bootstrap/dist/js/bootstrap.js",
                     "./node_modules/bootstrap-table/dist/bootstrap-table.js",
-                    "./node_modules/bootstrap-treeview/src/js/bootstrap-treeview.js",
+                    "./node_modules/bootstrap-treeview/dist/bootstrap-treeview.js",
                     "./node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js",
                     "./node_modules/bootstrap-validator/dist/validator.js",
                     "./node_modules/moment/moment.js",
-                    "./node_modules/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js",
+                    "./node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.js",
                     "./node_modules/underscore/underscore.js",
                     "./node_modules/backbone/backbone.js",
                     "./node_modules/highcharts/highcharts.js",
                     "./node_modules/qtip2/dist/jquery.qtip.js",
-                    "./node_modules/uri.js/src/URI.js",
+                    "./node_modules/urijs/src/URI.js",
                     "./node_modules/cookies-js/dist/cookies.js",
                     "./node_modules/crypto-js/crypto-js.js"
                 ],
@@ -84,8 +84,9 @@ module.exports = function(grunt) {
                     {   expand: true, cwd: "lib/jsorolla/src/lib/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib" },
                     {   expand: true, cwd: "lib/jsorolla/src/lib/cache/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/cache" },
                     {   expand: true, cwd: "lib/jsorolla/src/lib/clients/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/clients" },
-                    {   expand: true, cwd: "lib/jsorolla/src/lib/data-adapter/feature/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/data-adapter/feature" },
-                    {   expand: true, cwd: "lib/jsorolla/src/lib/opencga/variant/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/opencga/variant" },
+                    {   expand: true, cwd: "lib/jsorolla/src/lib/webcomponents/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/webcomponents" },
+                    {   expand: true, cwd: "lib/jsorolla/src/lib/webcomponents/opencga/variant/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/webcomponents/opencga/variant" },
+                    {   expand: true, cwd: "lib/jsorolla/src/lib/data-adapter/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/data-adapter" },
                     {   expand: true, cwd: "lib/jsorolla/src/lib/visualisation/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/visualisation" },
                     {   expand: true, cwd: "lib/jsorolla/src/lib/widgets/feature/info/", src: ["*.js"], dest: "<%= build.path %>/jsorolla/lib/widgets/feature/info" },
                     {   expand: true, cwd: "lib/jsorolla/src/lib/widgets/", src: ["ux-window.js"], dest: "<%= build.path %>/jsorolla/lib/widgets" },
@@ -100,7 +101,7 @@ module.exports = function(grunt) {
                     {   flatten: true, expand: true, cwd: "./node_modules", src: ["font-awesome/fonts/*"], dest: "<%= build.path %>/fonts" },
                     {   flatten: true, expand: true, cwd: "./lib", src: ["jsorolla/styles/css/style.css"], dest: "<%= build.path %>/css" },
                     {   flatten: true, expand: true, cwd: "./lib", src: ["jsorolla/styles/img/*"], dest: "<%= build.path %>/img" },
-                    {   expand: true, cwd: "./bower_components", src: ["polymer/*"], dest: "<%= build.vendor %>" },
+                    // {   expand: true, cwd: "./node_modules", src: ["@polymer/polymer/polymer-element.html"], dest: "<%= build.vendor %>" },
                     {   expand: true, cwd: "src", src: ["index.html"], dest: "<%= build.path %>/" },
                     {   expand: true, cwd: "src", src: ["conf/config.js"], dest: "<%= build.path %>/" },
                     {   expand: true, cwd: "src", src: ["conf/tools.js"], dest: "<%= build.path %>/" },
@@ -126,23 +127,23 @@ module.exports = function(grunt) {
                 options: {
                     // Task-specific options go here.
                     stripComments: true,
-                    excludes: ["bower_components/polymer/polymer.html"]
+                    // excludes: ["node_modules/@polymer/polymer/polymer-element.html"]
                 },
                 files: {
                     "<%= build.path %>/iva-app.html": "src/iva-app.html"
                 }
             }
         },
-        watch: {
-            files: ["<%= jshint.files %>"],
-            tasks: ["jshint"]
-        },
+        // watch: {
+        //     files: ["<%= jshint.files %>"],
+        //     tasks: ["jshint"]
+        // },
         replace: {
             dist: {
                 options: {
                     patterns: [
                         {
-                            match: /\.\.\/bower_components/g,
+                            match: /\.\.\/node_modules/g,
                             replacement: "vendor"
                         },
                         {
