42a4fa6058b7936d06a0978a6ebe2d0e1926c63f
Alexis Martínez Chacón <alemarcha26@gmail.com>
2017-11-17 09:40:08 +0100

    Create event to show Notify message from events

--- src/utils/utilsApp.js
+++ src/utils/utilsApp.js
@@ -1,73 +1,28 @@
 class UtilsApp {
 
     // Notify api for Iva. http://bootstrap-notify.remabledesigns.com/
-    static showNotify(options = {}, settings = {}) {
+    static showNotify(options = {}, settings = {}, message = "", type = "INFO") {
+        var types = {
+            'ERROR': 'danger',
+            'SUCCESS': 'success',
+            'WARNING': 'warning',
+            'INFO': 'info',
+        };
         let settingsDefault = {
             placement: {
                 from: "top",
                 align: "center"
-            }
+            },
+            type: types[type] || types['INFO']
         };
 
-        settingsDefault = Object.assign({}, settingsDefault, settings);
-        $.notify(options, settingsDefault);
-    }
-    static showNotifyError(message, options = {}, settings = {}) {
         let optionsDefault = {
-            icon: 'glyphicon glyphicon-warning-sign',
             message: message
-
-        };
-
-        let settingsDefault = {
-            type: 'danger'
         };
 
         settingsDefault = Object.assign({}, settingsDefault, settings);
         optionsDefault = Object.assign({}, optionsDefault, options);
-        this.showNotify(optionsDefault, settingsDefault);
+
+        $.notify(optionsDefault, settingsDefault);
     }
-
-    static showNotifySuccess(message, options = {}, settings = {}) {
-        let optionsDefault = {
-            icon: 'glyphicon glyphicon-warning-sign',
-            message: message
-        };
-
-        let settingsDefault = {
-            type: 'success'
-        };
-        settingsDefault = Object.assign({}, settingsDefault, settings);
-        optionsDefault = Object.assign({}, optionsDefault, options);
-        this.showNotify(optionsDefault, settingsDefault);
-    }
-
-    static showNotifyInfo(message, options = {}, settings = {}) {
-        let optionsDefault = {
-            icon: 'glyphicon glyphicon-warning-sign',
-            message: message
-        };
-
-        let settingsDefault = {
-            type: 'info'
-        };
-        settingsDefault = Object.assign({}, settingsDefault, settings);
-        optionsDefault = Object.assign({}, optionsDefault, options);
-        this.showNotify(optionsDefault, settingsDefault);
-    }
-
-    static showNotifyWarning(message, options = {}, settings = {}) {
-        let optionsDefault = {
-            icon: 'glyphicon glyphicon-warning-sign',
-            message: message
-        };
-
-        let settingsDefault = {
-            type: 'warning'
-        };
-        settingsDefault = Object.assign({}, settingsDefault, settings);
-        optionsDefault = Object.assign({}, optionsDefault, options);
-        this.showNotify(optionsDefault, settingsDefault);
-    }
-
 }
