c5b9f588652c57b838a9ac2817964bf91551360d
imedina <igmecas@gmail.com>
2017-12-08 00:28:24 +0000

    Add a new array to skip subsections from th filter menu

--- src/conf/tools.js
+++ src/conf/tools.js
@@ -23,10 +23,11 @@ const filter = {
     tooltip: {
         classes: "qtip-dark qtip-rounded qtip-shadow"
     },
-    sections: [
+    skipSubsections: [],    // controls which subsections are disabled and should not be displayed
+    sections: [             // sections and subsections, structure and order is respected
         {
             title: "Study and Cohorts",
-            collapsed: true,
+            collapsed: false,
             subsections: [
                 {
                     id: "sample",
@@ -238,7 +239,8 @@ const tools = {
                 },
             },
         ],
-        filter: filter,
+        // This disables two subsections in the filter menu Prioritization
+        filter: Object.assign({}, filter, {skipSubsections: ["cohort", "study"]}),
         grid: {
             showSelect: true,
             nucleotideGenotype: false
@@ -495,7 +497,11 @@ const tools = {
             },
         ],
         active: false,
-        filter: filter
+        filter: filter,
+        grid: {
+            showSelect: false,
+            nucleotideGenotype: false
+        }
     },
     genomeBrowser: {
         active: false,
