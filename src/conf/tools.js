26987493317ed4fd9f5a1383810e1114cc44e8fa
Alexis Martínez Chacón <alemarcha26@gmail.com>
2017-12-13 13:42:21 +0100

    Enabled showSelect for clinical section

--- src/conf/tools.js
+++ src/conf/tools.js
@@ -498,7 +498,7 @@ const tools = {
         active: false,
         filter: filter,
         grid: {
-            showSelect: false,
+            showSelect: true,
             nucleotideGenotype: true
         }
     },
