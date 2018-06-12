682d8705780194708de349ba9fd86c6c84e1b521
imedina <igmecas@gmail.com>
2018-06-12 23:51:43 +0100

    config: make GenomeBrowser hidden by default

--- src/conf/tools.js
+++ src/conf/tools.js
@@ -216,7 +216,7 @@ const tools = {
         title: "Variant Browser",
         active: false,
         showSummary: true,
-        showGenomeBrowser: true,
+        showGenomeBrowser: false,
         filter: {
             // This disables two subsections in the filter menu Prioritization
             menu: Object.assign({}, filterMenu, {skipSubsections: ["sample"]}),
