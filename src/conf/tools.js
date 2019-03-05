9fe8559c0776c8c7efcffd9a52f962310fdb7897
imedina <igmecas@gmail.com>
2019-03-05 14:03:58 +0000

    Fix welcome page X and Y chromosomes

--- src/conf/tools.js
+++ src/conf/tools.js
@@ -77,7 +77,7 @@ const filterMenu = {
                 },
                 {
                     id: "biotype",
-                    title: "Biotype",
+                    title: "Gene Biotype",
                     biotypes: [
                         "3prime_overlapping_ncrna", "IG_C_gene", "IG_C_pseudogene", "IG_D_gene", "IG_J_gene", "IG_J_pseudogene",
                         "IG_V_gene", "IG_V_pseudogene", "Mt_rRNA", "Mt_tRNA", "TR_C_gene", "TR_D_gene", "TR_J_gene", "TR_J_pseudogene",
