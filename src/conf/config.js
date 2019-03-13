b73a218c6d8c806b18d27182df827aa338b283c0
imedina <igmecas@gmail.com>
2019-03-13 05:32:53 +0000

    Upgrade version to 1.0.0-rc4

--- src/conf/config.js
+++ src/conf/config.js
@@ -79,7 +79,7 @@ const opencga = {
 
 const application = {
     title: "IVA",
-    version: "v1.0.0-rc3",
+    version: "v1.0.0-rc4",
     logo: "img/opencb-logo.png",
     // The order, title and nested submenus are respected
     menu: [
