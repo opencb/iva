a959d52ab73cc84fc30b4827884c912fdddc213e
imedina <igmecas@gmail.com>
2019-06-19 00:13:11 +0100

    Incremet version to 1.0.4-dev

--- src/conf/config.js
+++ src/conf/config.js
@@ -77,7 +77,7 @@ const opencga = {
 
 const application = {
     title: "IVA",
-    version: "v1.0.3-dev",
+    version: "v1.0.4-dev",
     logo: "img/opencb-logo.png",
     // The order, title and nested submenus are respected
     menu: [
