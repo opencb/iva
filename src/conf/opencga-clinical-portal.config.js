4641ed4ad546f6ad8266e2860d3c61f8a34daa7f
antonioaltamura <antonioaltamura7@gmail.com>
2020-07-15 11:26:54 +0100

    OpencgaClinicalPortalConfig updated

--- src/conf/opencga-clinical-portal.config.js
+++ src/conf/opencga-clinical-portal.config.js
@@ -1,3 +1,53 @@
 const OpencgaClinicalPortalConfig = {
     showCreate: false,
+    reviewCases: {
+        title: "Review Cases",
+        showTitle: true,
+        filter: {
+            sections: [
+                {
+                    title: "",
+                    fields: [
+                        {
+                            id: "case"
+                        },
+                        {
+                            id: "sample"
+                        },
+                        {
+                            id: "proband"
+                        },
+                        /*{
+                            id: "family"
+                        },*/
+                        {
+                            id: "disorder"
+                        },
+                        /*{
+                            id: "type"
+                        },
+                        {
+                            id: "status"
+                        },
+                        /*{
+                            id: "priority"
+                        },
+                        {
+                            id: "assignee"
+                        }*/
+                    ]
+                }
+            ],
+            examples: [
+
+            ]
+        },
+        grid: {
+            pageSize: 5,
+            pageList: [5, 10, 25],
+            detailView: false,
+            multiSelection: false,
+            showDeleteButton: false
+        }
+    }
 };
