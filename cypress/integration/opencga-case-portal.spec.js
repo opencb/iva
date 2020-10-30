a1b26ecaf1052c381a451b68e2cf3904e1e4c169
antonioaltamura <antonioaltamura7@gmail.com>
2020-10-30 17:34:10 +0100

    case portal E2E #217

--- cypress/integration/opencga-case-portal.spec.js
+++ cypress/integration/opencga-case-portal.spec.js
@@ -17,7 +17,7 @@
 
 import {login} from "../plugins/utils.js";
 
-context("File Browser", () => {
+context("Case Portal", () => {
     before(() => {
         login();
     })
@@ -26,11 +26,8 @@ context("File Browser", () => {
         cy.get("a[data-id=clinicalAnalysisPortal]", {timeout: 60000}).click({force: true})
         cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Case Portal")
 
-
         cy.get("div[data-cy='form-priority'] select").select(["Urgent", "High", "Medium", "Low"],{force: true}); //check all priorities
         cy.get("opencga-clinical-analysis-grid .bootstrap-table .fixed-table-container").find("tr[data-index]").should("have.length.gt", 1) //.should("be.gte", 1);
-
-        //cy.get("input[data-id=case]").type("")
 
         cy.get("opencga-clinical-analysis-grid .bootstrap-table .fixed-table-container tr[data-index=0]")
             .find("td:nth-child(1)")
@@ -59,6 +56,39 @@ context("File Browser", () => {
             })
 
         //cy.get("opencga-clinical-review-cases .rhs button", {timeout: 60000}).should("be.visible").and("contain", "Clear").click()
+        cy.get("button[data-cy='filter-button']").click({force:true})
+        cy.get(".saved-filter-wrapper a").contains("Clear").click({force:true})
+
+    })
+
+    it("check Columns togglability", () => {
+        cy.get("a[data-id=clinicalAnalysisPortal]", {timeout: 60000}).click({force: true})
+        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Case Portal")
+
+
+        cy.get("opencga-clinical-analysis-grid .columns-toggle-wrapper button").should("be.visible").and("contain", "Columns").click()
+        cy.get("opencga-clinical-analysis-grid .columns-toggle-wrapper ul li").and("have.length.gt", 1)
+
+
+        cy.get("opencga-clinical-analysis-grid .columns-toggle-wrapper ul li a").click( {multiple: true, timeout: 60000}) // deactivate all the columns
+        cy.get("opencga-clinical-analysis-grid .bootstrap-table .fixed-table-container tr[data-index=0]").find("td").should("have.lengthOf", 1);
+
+        cy.get("opencga-clinical-analysis-grid .columns-toggle-wrapper ul li a").click( {multiple: true, timeout: 60000}) // reactivate all the columns
+        cy.get("opencga-clinical-analysis-grid .bootstrap-table .fixed-table-container tr[data-index=0]").find("td").should("have.length.gt", 1);
+
+        /*cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").each(($li, index, $lis) => {
+            //Cypress.$("a", $li)
+        });*/
+
+        cy.get("opencga-clinical-analysis-grid .bootstrap-table .fixed-table-container tr[data-index=0]")
+            .find("td:nth-child(1)")
+            .then($div => {
+                const caseId = $div.text().trim()
+                cy.get("div[data-cy='form-case'] button").click()
+                cy.get("div[data-cy='form-case'] input").type(caseId + "{enter}",{force: true})
+
+                // TODO check clinical analysis view fields
+            })
 
     })
 
