2444041e03fbd53789b90ce98dce29443de8fb71
antonioaltamura <antonioaltamura7@gmail.com>
2020-11-11 17:54:16 +0100

    e2e directory eslint fix, minor bugfixes

--- cypress/integration/opencga-cohort-browser.spec.js
+++ cypress/integration/opencga-cohort-browser.spec.js
@@ -20,22 +20,22 @@ import {login} from "../plugins/utils.js";
 context("Cohort Browser", () => {
     before(() => {
         login();
-    })
+    });
 
     it("query", () => {
-        cy.get("a[data-id=cohort]", {timeout: 60000}).click({force: true})
-        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Family Browser")
+        cy.get("a[data-id=cohort]", {timeout: 60000}).click({force: true});
+        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Family Browser");
 
-        cy.get("opencga-cohort-grid .bootstrap-table .fixed-table-container").find("tr[data-index]").should("have.length.gte", 1) //.should("be.gte", 1);
+        cy.get("opencga-cohort-grid .bootstrap-table .fixed-table-container").find("tr[data-index]").should("have.length.gte", 1); // .should("be.gte", 1);
 
-    })
+    });
 
     it("aggregated query", () => {
-        cy.get("a[data-id=cohort]").click({force: true})
+        cy.get("a[data-id=cohort]").click({force: true});
 
-        cy.get("a[href='#facet_tab']").click({force: true})
+        cy.get("a[href='#facet_tab']").click({force: true});
         cy.get("button.default-facets-button").click();
         cy.get(".facet-wrapper .button-list button").should("have.length.gte", 1);
 
-    })
-})
+    });
+});
--- cypress/integration/opencga-variant-browser.spec.js
+++ cypress/integration/opencga-variant-browser.spec.js
@@ -20,53 +20,53 @@ import {login, randomString} from "../plugins/utils.js";
 context("Variant Browser", () => {
     before(() => {
         login();
-    })
+    });
 
     it("query", () => {
-        cy.get("a[data-id=browser]", {timeout: 60000}).click({force: true})
-        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Variant Browser") //should assertion comes from Chai and it follows its logic
+        cy.get("a[data-id=browser]", {timeout: 60000}).click({force: true});
+        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Variant Browser"); // should assertion comes from Chai and it follows its logic
 
-        cy.get("opencga-variant-grid .bootstrap-table .fixed-table-container").find("tr[data-index]").should("have.length.gt", 1) //.should("be.gte", 1);
+        cy.get("opencga-variant-grid .bootstrap-table .fixed-table-container").find("tr[data-index]").should("have.length.gt", 1); // .should("be.gte", 1);
 
         cy.get("input#lof").click({force: true});
         cy.get("opencga-active-filters").contains("Consequence Types 10");
         cy.get("button.ctActiveFilter").click();
-    })
+    });
 
     it("check Columns togglability", () => {
-        cy.get("a[data-id=browser]", {timeout: 60000}).click({force: true})
-        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Variant Browser")
+        cy.get("a[data-id=browser]", {timeout: 60000}).click({force: true});
+        cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Variant Browser");
 
-        cy.get("opencga-variant-grid .columns-toggle-wrapper button").should("be.visible").and("contain", "Columns").click()
-        cy.get("opencga-variant-grid .columns-toggle-wrapper ul li").and("have.length.gt", 1)
+        cy.get("opencga-variant-grid .columns-toggle-wrapper button").should("be.visible").and("contain", "Columns").click();
+        cy.get("opencga-variant-grid .columns-toggle-wrapper ul li").and("have.length.gt", 1);
 
-        cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").click( {multiple: true, timeout: 60000}) // deactivate all the columns
+        cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").click({multiple: true, timeout: 60000}); // deactivate all the columns
         cy.get("opencga-variant-grid .bootstrap-table .fixed-table-container tr[data-index=0]").find("td").should("have.lengthOf", 1);
 
-        cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").click( {multiple: true, timeout: 60000}) // reactivate all the columns
+        cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").click({multiple: true, timeout: 60000}); // reactivate all the columns
         cy.get("opencga-variant-grid .bootstrap-table .fixed-table-container tr[data-index=0]").find("td").should("have.length.gt", 1);
 
-        /*cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").each(($li, index, $lis) => {
+        /* cy.get("opencga-variant-grid .columns-toggle-wrapper ul li a").each(($li, index, $lis) => {
             //Cypress.$("a", $li)
         });*/
-    })
+    });
 
     it("aggregated query", () => {
-        cy.get("a[data-id=browser]").click({force: true})
-        cy.get("a[href='#facet_tab']").click({force: true})
-        cy.get("button.default-facets-button").click()
-        cy.get("div.search-button-wrapper button").click()
+        cy.get("a[data-id=browser]").click({force: true});
+        cy.get("a[href='#facet_tab']").click({force: true});
+        cy.get("button.default-facets-button").click();
+        cy.get("div.search-button-wrapper button").click();
 
-        //cy.wait(2000);
+        // cy.wait(2000);
 
-        cy.get("#bs-select-1-4").click({force: true}) // gene aggregation field
-        cy.get("#type_Select a").contains( "INSERTION").click({force: true})
-        cy.get("div.search-button-wrapper button").click()
+        cy.get("#bs-select-1-4").click({force: true}); // gene aggregation field
+        cy.get("#type_Select a").contains("INSERTION").click({force: true});
+        cy.get("div.search-button-wrapper button").click();
 
-    })
+    });
 
     // TODO fix bugs
-    /*it("check Filter controls", () => {
+    /* it("check Filter controls", () => {
         cy.get("a[data-id=browser]", {timeout: 60000}).click({force: true})
         cy.get("div.page-title h2", {timeout: 60000}).should("be.visible").and("contain", "Variant Browser")
 
@@ -107,4 +107,4 @@ context("Variant Browser", () => {
         //cy.get('.gene-tooltip').should('be.visible')
 
     })*/
-})
+});
