4bcdd9f0dcd9e32da58b5e8ed948f42b7d32b775
imedina <igmecas@gmail.com>
2020-11-26 01:11:51 +0000

    Upgrade JSorolla dependency

--- src/conf/opencga-variant-constants.js
+++ src/conf/opencga-variant-constants.js
@@ -18,7 +18,7 @@
 // export const biotypes = VARIANT_CONSTANTS.biotypes;
 const CHROMOSOMES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y", "MT"];
 
-const biotypes = [
+const BIOTYPES = [
     "3prime_overlapping_ncrna", "IG_C_gene", "IG_C_pseudogene", "IG_D_gene", "IG_J_gene", "IG_J_pseudogene",
     "IG_V_gene", "IG_V_pseudogene", "Mt_rRNA", "Mt_tRNA", "TR_C_gene", "TR_D_gene", "TR_J_gene", "TR_J_pseudogene",
     "TR_V_gene", "TR_V_pseudogene", "antisense", "lincRNA", "miRNA", "misc_RNA", "non_stop_decay",
