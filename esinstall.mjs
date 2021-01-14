01297a8a4f7c8cb5ed858119b676b4dad124d683
antonioaltamura <antonioaltamura7@gmail.com>
2021-01-14 18:33:45 +0100

    Snowpack dep replaced with esinstall

---
+++ esinstall.mjs
@@ -0,0 +1,15 @@
+/**
+ * esinstall configuration file
+ * https://github.com/snowpackjs/snowpack/tree/main/esinstall
+ *
+ */
+import {install} from 'esinstall';
+
+await install([
+    "lit-element",
+    "lit-html",
+    "lit-html/directives/class-map.js",
+    "lit-html/directives/if-defined.js"
+], {
+    /*options*/
+});
