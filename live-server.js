e0990ff6b82ea58e14243d9f4ef798996778584a
antonioaltamura <antonioaltamura7@gmail.com>
2021-02-03 13:11:08 +0100

    live-server fix and package.json `serve` script replaced

--- live-server.js
+++ live-server.js
@@ -1,13 +1,12 @@
 const liveServer = require("live-server");
 
 const params = {
-    port: 3000, // Set the server port. Defaults to 8080.
-    host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    port: 3000,
+    host: "localhost",
     open: "/src",
-    watch: "src,lib/jsorolla/src,lib/jsorolla/styles",
-    //ignore: "cypress,report,build,docker",
+    watch: ["src", "lib/jsorolla/src", "lib/jsorolla/styles"],
+    // ignore: "cypress,report,build,docker",
     // wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-    logLevel: 2 // 0 = errors only, 1 = some, 2 = lots
-    // middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+    logLevel: 2
 };
 liveServer.start(params);
