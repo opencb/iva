b4f44fcae539aa53f7efe5a8609e714bc25615bc
antonioaltamura <antonioaltamura7@gmail.com>
2021-02-10 17:23:51 +0100

    updating version

--- src/conf/config.js
+++ src/conf/config.js
@@ -109,7 +109,7 @@ const opencga = {
 
 const application = {
     title: "IVA",
-    version: "v2.1.0",
+    version: "v2.1.0-RC",
     logo: "img/iva.svg",
     companyLogo: "img/Genomics-England-logo-2015-white.png",
     mode: "development",
