0beefbd848e5093dba642c11cd48ecbb41b1fbd7
antonioaltamura <antonioaltamura7@gmail.com>
2021-02-23 12:23:16 +0100

    opencga-variant-constants BIOTYPES list changed

--- src/conf/opencga-variant-constants.js
+++ src/conf/opencga-variant-constants.js
@@ -18,14 +18,9 @@
 // export const biotypes = VARIANT_CONSTANTS.biotypes;
 const CHROMOSOMES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y", "MT"];
 
-const BIOTYPES = [
-    "3prime_overlapping_ncrna", "IG_C_gene", "IG_C_pseudogene", "IG_D_gene", "IG_J_gene", "IG_J_pseudogene",
-    "IG_V_gene", "IG_V_pseudogene", "Mt_rRNA", "Mt_tRNA", "TR_C_gene", "TR_D_gene", "TR_J_gene", "TR_J_pseudogene",
-    "TR_V_gene", "TR_V_pseudogene", "antisense", "lincRNA", "miRNA", "misc_RNA", "non_stop_decay",
-    "nonsense_mediated_decay", "polymorphic_pseudogene", "processed_pseudogene", "processed_transcript",
-    "protein_coding", "pseudogene", "rRNA", "retained_intron", "sense_intronic", "sense_overlapping", "snRNA",
-    "snoRNA", "transcribed_processed_pseudogene", "transcribed_unprocessed_pseudogene",
-    "translated_processed_pseudogene", "unitary_pseudogene", "unprocessed_pseudogene"
+const BIOTYPES = ["protein_coding", "nonsense_mediated_decay", "lncRNA", "non_stop_decay", "lincRNA",
+    "3prime_overlapping_ncrna", "bidirectional_promoter_lncRNA", "miRNA", "retained_intron",
+    "snRNA", "snoRNA", "non_coding", "antisense", "sense_intronic", "sense_overlapping"
 ];
 
 const VARIANT_TYPES = ["SNV", "INDEL", "COPY_NUMBER", "INSERTION", "DELETION", "MNV", "SV"];
