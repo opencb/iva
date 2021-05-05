2bbcbf1e6e247365a5814138cac7c7cea3d82e4c
antonioaltamura <antonioaltamura7@gmail.com>
2021-05-05 18:24:32 +0200

    E2E utils.js improvements #214

--- cypress/plugins/utils.js
+++ cypress/plugins/utils.js
@@ -16,6 +16,11 @@ Cypress.Commands.add("getAttached", selector => {
     }).then(() => cy.wrap($el));
 });
 
+export const waitTable = gridSelector => {
+    cy.wait(1000); // it is necessary to avoid the following negative assertion is early satisfied
+    cy.get(gridSelector + " div.fixed-table-loading", {timeout: 60000}).should("be.not.visible");
+};
+
 export const login = () => {
     cy.visit("http://localhost:3000/src/#login");
     const username = Cypress.env("username");
@@ -55,6 +60,7 @@ export const waitTableResults = gridSelector => {
  * it check the table actually contains a single result
  */
 export const checkExactResult = (gridSelector, numResults = 1) => {
+    waitTable(gridSelector);
     cy.get(gridSelector + " table", {timeout: 60000}).find("tr[data-index]", {timeout: 60000}).should("have.lengthOf", numResults); // .should("be.gte", 1);
 };
 
@@ -62,6 +68,7 @@ export const checkExactResult = (gridSelector, numResults = 1) => {
  * it check the table actually contains results
  */
 export const checkResults = gridSelector => {
+    waitTable(gridSelector);
     cy.get(gridSelector + " table", {timeout: 60000}).find("tr[data-index]", {timeout: 60000}).should("have.length.gt", 0); // .should("be.gte", 1);
 };
 
@@ -69,9 +76,7 @@ export const checkResults = gridSelector => {
  * it check the table contains results or the message "No matching records found"
  */
 export const checkResultsOrNot = (gridSelector, id) => {
-    cy.wait(1000); // it is necessary to avoid the following negative assertion is early satisfied
-    cy.get(gridSelector + " div.fixed-table-loading", {timeout: 60000}).should("be.not.visible");
-
+    waitTable(gridSelector);
     cy.get(gridSelector + " .fixed-table-body > table > tbody", {timeout: 60000}).find(" > tr", {timeout: 10000})
         .should("satisfy", $els => {
 
@@ -82,10 +87,6 @@ export const checkResultsOrNot = (gridSelector, id) => {
 
             const $firstRow = Cypress.$($els[0]);
             if ($firstRow) {
-                // console.error("$firstRow.data(index)", $firstRow.data("index"))
-                // console.error("$els.text()", $els.text())
-                // console.error("id", id)
-                // console.error("No matching records found", $els.text().includes("No matching records found"))
                 // it covers either the case of some results or 0 results
                 return $firstRow.data("index") === 0 || $els.text().includes("No matching records found");
             }
@@ -94,11 +95,14 @@ export const checkResultsOrNot = (gridSelector, id) => {
 };
 
 /**
- * given column and row coordinates, it returns a single value out of a bootstrap table
- *
+ * Given column and row coordinates, it returns the value of a single cell out of a bootstrap table
+ * @param {String} gridSelector CSS selector of the table
+ * @param {Number} colIndex column index
+ * @param {Number} rowIndex row index
+ * @param {String} invokeFn text|html
+ * @return {Cypress.Chainable}
  */
 export const getResult = (gridSelector, colIndex = 0, rowIndex = 0, invokeFn= "text") => {
-    // check results are >= resultIndex
     // cy.get(gridSelector + " table", {timeout: 60000}).find("tr[data-index]", {timeout: 60000}).should("have.length.gte", rowIndex);
     // cy.get(gridSelector + " table", {timeout: 60000}).find(`tr[data-index=${rowIndex}] > :nth-child(${colIndex})`, {timeout: 60000}).invoke("text").as("text")
     return cy.get(gridSelector + " table", {timeout: 60000}).find(`tr[data-index=${rowIndex}] > :nth-child(${colIndex + 1})`, {timeout: 60000}).first().invoke(invokeFn);
