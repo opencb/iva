6b42feb0cbb3f3f9f865e4b5e5167cd83fe143c4
antonioaltamura <antonioaltamura7@gmail.com>
2021-06-21 15:22:33 +0200

    entrypoint.sh host param added #210

--- docker/iva-app/entrypoint.sh
+++ docker/iva-app/entrypoint.sh
@@ -4,3 +4,18 @@ echo "total args:" $#
 echo "first:" $1
 echo "second:" $2
 #sed -i 's/host:".*"/host: "val" /g' /usr/local/apache2/htdocs/iva/conf/conf.js
+
+
+while [[ $# -gt 0 ]] ;do
+   case $1 in
+      --host=*)
+         echo host ${1##--host=}
+         echo "opencga.host = ${1##--host=};" >> /usr/local/apache2/htdocs/iva/conf/conf.js
+         # exit
+         ;;
+   esac
+   shift
+done
+
+echo CMD"$@"
+exec "$@"
