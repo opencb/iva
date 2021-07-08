41643be0ef32a30cb4b49523202d3869b7a36781
antonioaltamura <antonioaltamura7@gmail.com>
2021-07-08 14:00:32 +0200

    opencga-clinical-review-cases.settings.js completed opencga/iva#236 opencga/jsorolla#152

--- src/conf/opencga-clinical-review-cases.settings.js
+++ src/conf/opencga-clinical-review-cases.settings.js
@@ -1,5 +1,6 @@
 const OpencgaClinicalReviewCasesSettings = {
     menu: {
+        // merge criterium: it uses this `filters` array as filter for internal one. Each object is merged with spread operator
         filters: [
             {
                 id: "case"
@@ -23,6 +24,7 @@ const OpencgaClinicalReviewCasesSettings = {
                 id: "assignee"
             }
         ],
+        // merge criterium: full outer join like. it adds objects presents in internal array only and in external array only. In case of objects with same id, the external values overwrite the internal.
         examples: [
             {
                 id: "Intellectual disability2",
@@ -34,10 +36,15 @@ const OpencgaClinicalReviewCasesSettings = {
         ]
     },
     table: {
-        /*toolbar: {
-            showCreate: true
-        },*/
-        toolbarButtons: ["columns", "download"],
+        // merge criterium: spread operator
+        toolbar: {
+            showColumns: true,
+            showCreate: true,
+            showExport: false,
+            showDownload: true,
+            // columns list will be added in grid components based on settings.table.columns
+        },
+        // merge criterium: uses this array as filter for internal 1D/2D array. It handles row/col span
         columns: ["caseId", "probandId", "familyId", "disorderId", "interpretation", "action"]
     }
 };
