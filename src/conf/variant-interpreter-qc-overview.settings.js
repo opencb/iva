83a0f0ea4532447de1bb55e96f27d1c3c326c377
antonioaltamura <antonioaltamura7@gmail.com>
2021-07-14 16:54:47 +0200

    Variant Interpreter Qc Overview Settings added #236

---
+++ src/conf/variant-interpreter-qc-overview.settings.js
@@ -0,0 +1,4 @@
+const variantInterpreterQcOverviewSettings = {
+    // for both cancer and family tabs
+    tabs: ["Summary", /* "VariantStats", "SamtoolsPlots", /* "Alignment",*/ "InferredSex", "MendelianErrors", "Relatedness", /* "AlignmentStats",*/ "GenomicContext"]
+}
