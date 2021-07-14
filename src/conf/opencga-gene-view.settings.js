84aba94b37a683ab8c9ff953a280c8ee30c460ff
antonioaltamura <antonioaltamura7@gmail.com>
2021-07-14 16:55:16 +0200

    Gene View Settings in progress #236

---
+++ src/conf/opencga-gene-view.settings.js
@@ -0,0 +1,5 @@
+const opencgaGeneViewSettings = {
+    // set to true if server on which IVA is installed is exposed to Internet (genomemaps.org, ensembl.org).
+    externalLinks: false
+    // TODO add configuration from conf/tools
+};
