c1deb66e26d8dffa5ea455476d67b0cd6c94f7a2
antonioaltamura <antonioaltamura7@gmail.com>
2021-07-15 18:20:59 +0200

    Catalog settings.js completed #236

---
+++ src/conf/opencga-cohort-browser.settings.js
@@ -0,0 +1,39 @@
+const opencgaCohortBrowserSettings = {
+    /* menu: {
+        // merge criterium: it uses this `filters` array as filter for internal one. Each object is merged with spread operator
+        filters: [
+            {
+                id: "id"
+            },
+            {
+                id: "samples"
+            },
+            {
+                id: "type"
+            },
+            {
+                id: "date"
+            },
+            {
+                id: "annotations"
+            }
+        ],
+        // merge criterium: full outer join-like. it adds objects presents in internal array only and in external array only. In case of same id, the external value overwrite the internal.
+        examples: [
+        ]
+    },
+    table: {
+        // merge criterium: spread operator
+        toolbar: {
+            showColumns: true,
+            showExport: false,
+            showDownload: true
+            // columns list for the dropdown will be added in grid components based on settings.table.columns
+        },
+        // merge criterium: uses this array as filter for internal 1D/2D array. It handles row/col span
+        columns: ["id", "numSamples", "creationDate", "type"]
+    },
+    // merge criterium: uses this array as filter for internal 1D array.
+    detail: ["cohort-view", "sample-view", "json-view"]
+    */
+};
