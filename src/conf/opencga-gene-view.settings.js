26552cf73b8f3fdc3185a0c2b446ab7057ffb870
antonioaltamura <antonioaltamura7@gmail.com>
2021-07-16 14:06:17 +0200

    family-view and gene-view settings added #236

--- src/conf/opencga-gene-view.settings.js
+++ src/conf/opencga-gene-view.settings.js
@@ -1,5 +1,17 @@
 const opencgaGeneViewSettings = {
-    // set to true if server on which IVA is installed is exposed to Internet (genomemaps.org, ensembl.org).
-    externalLinks: false
-    // TODO add configuration from conf/tools
+    // set to true if the server on which IVA is running is exposed to Internet (for genomemaps.org, ensembl.org).
+    externalLinks: true,
+    // from conf/tools.js
+    protein: {
+        color: {
+            synonymous_variant: "blue",
+            coding_sequence_variant: "blue",
+            missense_variant: "orange",
+            protein_altering_variant: "orange",
+            start_lost: "red",
+            stop_gained: "red",
+            stop_lost: "red",
+            stop_retained_variant: "red"
+        }
+    }
 };
