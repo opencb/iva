f1c16dd684883d3b676e7f7e77d8701512461ff1
antonioaltamura <antonioaltamura7@gmail.com>
2021-08-04 14:20:54 +0200

    dependency-graph.sh script added

---
+++ dependency-graph.sh
@@ -0,0 +1,20 @@
+exclude=\
+"/node_modules|"\
+"/web_modules|"\
+"lib/jsorolla/node_modules|"\
+"lib/jsorolla/src/genome-browser|"\
+"lib/jsorolla/src/core/webcomponents/loading-spinner.js|"\
+"lib/jsorolla/src/core/NotificationUtils.js|"\
+"lib/jsorolla/src/core/utils.js|"\
+"lib/jsorolla/src/core/utilsNew.js|"\
+"lib/jsorolla/src/core/webcomponents/PolymerUtils.js|"\
+"lib/jsorolla/src/core/clients|"\
+"lib/jsorolla/src/core/visualisation|"\
+"lib/jsorolla/src/core/webcomponents/opencga/clinical/obsolete|"\
+"lib/jsorolla/src/core/webcomponents/commons/filters/deprecated|"\
+"lib/jsorolla/src/core/webcomponents/variant/deprecated|"\
+"./deprecated|"\
+"lib/jsorolla/src/core/webcomponents/Notification.js"
+
+depcruise "lib/jsorolla/src/core/webcomponents/**/*.js" -x "^($exclude)" --output-type dot | dot -T svg > dependency.svg
+depcruise "lib/jsorolla/src/core/webcomponents/**/*.js" -x "^($exclude)" --output-type json > dependency.json
